2e04f859411731304c30988598e29a38002140b9
jt-lowe <jtlowe6@gmail.com>
2022-02-07 22:01:20 +1100

    updated Python and SQL files

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
--- Create tables for raw data to be loaded into
+-- Create tables for Pandas Dataframes to be loaded into
 CREATE TABLE ratings (
 Game_Title TEXT,
 Platform TEXT,
@@ -13,9 +13,16 @@ Platform TEXT,
 Global_Sales TEXT
 );
 
+CREATE TABLE merged (
+Game_Title TEXT,
+Platform TEXT,
+ESRB_Rating TEXT,
+Metascore INT,
+User_Score DECIMAL,
+Global_Sales TEXT
+);
 
-
--- Joins tables
+-- Joins tables as proof of import
 SELECT ratings.Game_Title, ratings.platform, ratings.ESRB_Rating, ratings.Metascore, ratings.User_Score, sales.Global_Sales
 FROM ratings
 JOIN sales
